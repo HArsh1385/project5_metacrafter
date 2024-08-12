d3db2b895aaa3b9dd613f73e58dcd9cafb121938
naman221462 <142684749+naman221462@users.noreply.github.com>
2024-08-13 00:58:36 +0530

    Update 4_Smart_Cntract.sol

--- 4_Smart_Cntract.sol
+++ 4_Smart_Cntract.sol
@@ -1,31 +1,33 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+//For this project, you will write a smart contract to create your own ERC20 token
+//Only contract owner should be able to mint tokens
+//Any user can transfer tokens
+//Any user can burn tokens
+// contract/bitLand.sol
+//SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.17;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.5/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.5/contracts/access/Ownable.sol";
-
-contract GoldLandToken is ERC20, Ownable {
-    constructor(uint256 initialSupply) ERC20("GoldLand", "GLD") {
-        _mint(msg.sender, initialSupply);
+contract BitLandToken is ERC20, Ownable {
+    constructor(uint256 initialBalance) ERC20("BitLand", "BITL") {
+        _mint(msg.sender, initialBalance);
     }
 
-    // Function for the owner to mint new tokens
-    function mint(address recipient, uint256 amount) external onlyOwner {
-        require(recipient != address(0), "Invalid address");
-        require(amount > 0, "Amount must be greater than zero");
-        _mint(recipient, amount);
+    function mint(address to, uint256 amount) public onlyOwner {
+        require(to != address(0), "ERC20: mint to the zero address");
+        require(amount > 0, "ERC20: mint amount must be greater than zero");
+        _mint(to, amount);
     }
 
-    // Function for users to burn their own tokens
-    function burn(uint256 amount) external {
-        require(amount > 0, "Amount must be greater than zero");
+    function burn(uint256 amount) public {
+         require(amount > 0, "ERC20: burn amount must be greater than zero");
         _burn(msg.sender, amount);
     }
 
-    // Overriding the transfer function to include additional checks
     function transfer(address recipient, uint256 amount) public override returns (bool) {
-        require(recipient != address(0), "Invalid address");
-        require(amount > 0, "Amount must be greater than zero");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(amount > 0, "ERC20: transfer amount must be greater than zero");
         return super.transfer(recipient, amount);
     }
+
 }
